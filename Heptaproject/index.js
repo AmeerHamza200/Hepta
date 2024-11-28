7dba2337b443d6187a4101b90c816131b932849c
AmeerHamza200 <ameerhamzarj741@gmail.com>
2024-11-28 10:45:55 +0500

    Add files via upload

---
+++ Heptaproject/index.js
@@ -0,0 +1,30 @@
+// JavaScript to toggle the menu visibility
+document.getElementById("hamburger").addEventListener("click", function() {
+  const menu = document.getElementById("menu");
+  menu.classList.toggle("active");
+});
+
+
+// JavaScript code for the slider functionality
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slides img');
+    let currentSlide = 0;
+    
+    // Initialize by showing the first slide
+    slides[currentSlide].classList.add('active');
+  
+    function showSlides() {
+      // Remove "active" class from all slides
+      slides.forEach(slide => slide.classList.remove('active'));
+      
+      // Add "active" class to the current slide
+      slides[currentSlide].classList.add('active');
+      
+      // Move to the next slide
+      currentSlide = (currentSlide + 1) % slides.length;
+    }
+  
+    // Run the slideshow every 2 seconds
+    setInterval(showSlides, 2000);
+  });
+  
